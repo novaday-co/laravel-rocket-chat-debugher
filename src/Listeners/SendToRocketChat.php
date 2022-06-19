9dc6ab9cafad82b6623d0e4c5894273ee9959bd7
hosein.xz <h.askari.16@gmail.com>
2022-06-19 11:39:34 +0430

    refactor: catch ConnectException

--- src/Listeners/SendToRocketChat.php
+++ src/Listeners/SendToRocketChat.php
@@ -3,6 +3,7 @@
 namespace Novaday\Debugher\Listeners;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ConnectException;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Novaday\Debugher\Events\LogPushed;
 use Novaday\Debugher\Logger;
@@ -11,11 +12,12 @@ class SendToRocketChat implements ShouldQueue{
 
     public function handle(LogPushed $event)
     {
+
         try {
             $client = new Client(Logger::getHttpHeaders());
             $body['text'] = $event->message;
             $client->post( config()->get('debugher.end_point'),  ['form_params'=> $body]);
-        } catch (HttpException $exception) {
+        } catch (ConnectException $exception) {
             \Log::error($exception->getMessage());
         }
     }
